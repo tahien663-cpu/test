97f17f495a34fd05cad3b9122986fd83cc54e728
tahien663-cpu <tahien663@gmail.com>
2025-10-01 13:57:32 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['smooth-masks-fix.loca.lt'], // 👈 Add this line
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001', // Ensure this matches your backend port
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  base: '/',
 });
